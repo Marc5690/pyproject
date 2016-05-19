5f14b88f975a7b8f8d674e5f5a3f8b86078a899f
marclaffan@gmail.com <marclaffan@gmail.com>
2016-05-19 13:11:15 +0100

    Added Recommender starting place

--- Connection.py
+++ Connection.py
@@ -1,10 +1,8 @@
 import tweepy
 
-from bottle import route, run, template
+from recommender import Recommender
 
-#auth = tweepy.OAuthHandler("", "")
-#auth.set_access_token("", "")
-#api = tweepy.API(auth)
+from bottle import route, run, template
 
 #Use the following url to use my (Marc's) details to login with all the keys/tokens and get back my first tweet:
 #localhost:8088/auth/oj8A0mh2hMMLZBAYr9hrsyGXK/vEbApM5duqpSj3fu0oYDmuIMSDlcefk5wToHwmt5DByrqpTR3x/3088841945-9TzefMYW385iw5jsPSo7NeujLn6OigE3ZBC86hu/bve9QIWzyFUlMn4BOVZemcnZ8e3GjhwmuVViz8QhwoRse
@@ -22,7 +20,11 @@ def authentication(ckey, csecret, atoken, atokensecret):
  api = tweepy.API(auth)
  my_tweets = api.user_timeline()
  my_first_tweet = my_tweets[0].text
- return template('My first Tweet was: {{my_first_tweet_here}}', my_first_tweet_here = my_first_tweet)
+ following = api.followers()
+
+ recommenderObj = Recommender()
+ generatedTweet = recommenderObj.generate(my_tweets, 1, following, 2)
+
+ return template('My first Tweet was: {{my_first_tweet_here}}, my generated text is {{generatedTweetHere}}', my_first_tweet_here = my_first_tweet, generatedTweetHere =generatedTweet)
 
 run(host='localhost', port=8088, debug=True)
-
