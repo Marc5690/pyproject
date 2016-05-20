25fe84ab02584b168c1a08d32b8e0397855b99fd
marclaffan@gmail.com <marclaffan@gmail.com>
2016-05-20 16:57:17 +0100

    Technically recommending 'Marc Laffan's first tweet now

--- Connection.py
+++ Connection.py
@@ -1,11 +1,12 @@
 import tweepy
 
 from recommender import Recommender
+from collections import defaultdict
 
 from bottle import route, run, template
 
-#Use the following url to use my (Marc's) details to login with all the keys/tokens and get back my first tweet:
-#localhost:8088/auth/oj8A0mh2hMMLZBAYr9hrsyGXK/vEbApM5duqpSj3fu0oYDmuIMSDlcefk5wToHwmt5DByrqpTR3x/3088841945-9TzefMYW385iw5jsPSo7NeujLn6OigE3ZBC86hu/bve9QIWzyFUlMn4BOVZemcnZ8e3GjhwmuVViz8QhwoRse
+#Use the following url to use the test accounts details to login with all the keys/tokens
+#localhost:8088/auth/KOUIbWm4VWYzI0uuQLogzGRa0/r5Ac1fwLmuYFYL6biR4E1iYzS8S78DInUNM3AQ76EeMDBBVSFL/733308744638038017-oZYXhQOz1qUgTe2Sex3PctTMbkfM1dJ/3jAoAPk2krE9KClg4XC0MIDLlpAMKUumi6cDSnf5gtWJk
 
 @route('/auth/<ckey>/<csecret>/<atoken>/<atokensecret>')
 def authentication(ckey, csecret, atoken, atokensecret):
@@ -20,11 +21,18 @@ def authentication(ckey, csecret, atoken, atokensecret):
  api = tweepy.API(auth)
  my_tweets = api.user_timeline()
  my_first_tweet = my_tweets[0].text
- following = api.followers()
-
+ following = api.friends()
+ #d = defaultdict(list)
+ dict_of_followed_tweets = {}
+ for friend in following:
+   follow_acc = api.get_user(friend.screen_name)
+   dict_of_followed_tweets[friend.screen_name] = friend.timeline()
+   #x = [method for method in dir(friend) if callable(getattr(friend, method))]
+ 
+ #return template('My fi {{dict_of_followed_tweets}}', dict_of_followed_tweets=dict_of_followed_tweets)
  recommenderObj = Recommender()
- generatedTweet = recommenderObj.generate(my_tweets, 1, following, 2)
+ generatedTweet = recommenderObj.generate(my_tweets, 1, following, 2, dict_of_followed_tweets)
 
- return template('My first Tweet was: {{my_first_tweet_here}}, my generated text is {{generatedTweetHere}}', my_first_tweet_here = my_first_tweet, generatedTweetHere =generatedTweet)
+ return template('Result: {{generatedTweetHere}}',generatedTweetHere =generatedTweet)
 
 run(host='localhost', port=8088, debug=True)
